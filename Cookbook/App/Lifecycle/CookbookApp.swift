dee3802792e636ced642dc6778d8f644ac8b1164
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-01 19:13:45 +0100

    ContentView name refactorized to RecipeView

--- Cookbook/App/Lifecycle/CookbookApp.swift
+++ Cookbook/App/Lifecycle/CookbookApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CookbookApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RecipesView()
         }
     }
 }
--- Cookbook/Presentation/Scenes/Recipes/RecipesView.swift
+++ Cookbook/Presentation/Scenes/Recipes/RecipesView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RecipesView: View {
     var body: some View {
         Text("Recipes")
     }
