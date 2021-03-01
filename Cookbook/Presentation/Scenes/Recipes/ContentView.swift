92079be9774d9925cfb91cc87afc5d12c0f41f7f
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-01 19:09:47 +0100

    Architecture created

---
+++ Cookbook/Presentation/Scenes/Recipes/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  Cookbook
+//
+//  Created by Raúl Pera Pairó on 1/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Recipes")
+    }
+}
