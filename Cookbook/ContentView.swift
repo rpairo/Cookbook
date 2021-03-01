4bcc83c9cc20bdce40abc1fde59669ae1de2a0a4
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-01 18:56:53 +0100

    Initial Commit

---
+++ Cookbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cookbook
+//
+//  Created by Raúl Pera Pairó on 1/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Raúl Pera Pairó on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
