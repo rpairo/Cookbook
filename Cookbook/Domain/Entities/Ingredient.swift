22e8e5b2c6db46820e1bf327d139096436fd88f9
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-02 14:57:22 +0100

    Fetch recipes use case created

---
+++ Cookbook/Domain/Entities/Ingredient.swift
@@ -0,0 +1,14 @@
+//
+//  Ingredient.swift
+//  Cookbook
+//
+//  Created by Raúl Pera Pairó on 1/3/21.
+//
+
+import Foundation
+
+struct Ingredient: Identifiable {
+    // MARK: Properties
+    var id: UUID
+    var name: String
+}
